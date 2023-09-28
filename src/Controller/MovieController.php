174914a157a86f888c20640efb5cc63c6b12f36a
Adrien Morin <morin.adrien@gmail.com>
2023-09-28 17:03:32 +0200

    Liliane -> for you

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MovieController extends AbstractController
 {
-    #[Route('/movie', name: 'movie_index')]
+    #[Route('/movie', name: 'movie_index', methods: 'get')]
     public function index(MovieRepository $movieRepository): Response
     {
         $movies = $movieRepository->findAll();
@@ -23,7 +23,15 @@ class MovieController extends AbstractController
         ]);
     }
 
-    #[Route('/movie/new', name: 'movie_new')]
+    #[Route('/movie/{id}', name: 'movie_show', requirements: ['id' => '\d{1,3}'], methods: 'get')]
+    public function show(Movie $movie): Response
+    {
+        return $this->render('movie/show.html.twig', [
+           'movie' => $movie
+        ]);
+    }
+
+    #[Route('/movie/new', name: 'movie_new', methods: ['get', 'post'])]
     public function new(Request $request, EntityManagerInterface $manager): Response
     {
         $movie = new Movie();
@@ -39,6 +47,26 @@ class MovieController extends AbstractController
 
         return $this->render('movie/new.html.twig', [
             'form' => $form
+        ]);
+    }
+
+    #[Route('/movie/update/{id}', name: 'movie_update', requirements: ['id' => '\d{1,3}'], methods: ['get', 'post'])]
+    public function update(Movie $movie, Request $request, EntityManagerInterface $manager): Response
+    {
+        $form = $this->createForm(MovieType::class, $movie);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->flush();
+
+            return $this->redirectToRoute('movie_update', [
+                'id' => $movie->getId()
+            ]);
+        }
+
+        return $this->render('movie/update.html.twig', [
+            'form' => $form,
+            'movie' => $movie
         ]);
     }
 }
